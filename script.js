1c1cd6573aa741fee4a315c770b28271743f80cb
hoken92 <hkenneth92@gmail.com>
2024-09-16 21:01:02 -0700

    added part 2 solution

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Part One
 
 const csvString =
-  "Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232";
+  "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
 
 let cell1 = "";
 let cell2 = "";
@@ -52,3 +52,16 @@ for (let i = 0; i < csvString.length; i++) {
     console.log(cell1, cell2, cell3, cell4);
   }
 }
+
+// Part two
+
+// Counts how many columns and stored in columns
+let cols = csvString.split("\n");
+
+let colsArray = [];
+
+for (let i = 0; i < cols.length; i++) {
+  let cells = cols[i].split(",");
+  colsArray.push(cells);
+}
+console.log(colsArray);
