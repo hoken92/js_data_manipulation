9845aff57f8905146dae1e1e7f42d1941d4d60a7
hoken92 <hkenneth92@gmail.com>
2024-09-17 19:22:44 -0700

    added solution for part 3

--- script.js
+++ script.js
@@ -49,11 +49,12 @@ for (let i = 0; i < csvString.length; i++) {
     (currentCell === 4 && csvString[i + 1] === "\n") ||
     i + 1 === csvString.length
   ) {
-    console.log(cell1, cell2, cell3, cell4);
+    // console.log(cell1, cell2, cell3, cell4);
   }
 }
 
 // Part two
+// ==============================
 
 // Counts how many columns and stored in columns
 let cols = csvString.split("\n");
@@ -64,4 +65,58 @@ for (let i = 0; i < cols.length; i++) {
   let cells = cols[i].split(",");
   colsArray.push(cells);
 }
-console.log(colsArray);
+
+// Part three
+// ==============================
+
+let directory = [];
+
+// find a way to grab the first array, turn it into all lower case. use a form of iteration to put them into objects
+
+let headers = [];
+
+for (let i = 0; i < colsArray[0].length; i++) {
+  let cell = colsArray[0][i];
+  headers.push(cell.toLowerCase());
+}
+
+// console.log(headers);
+
+for (let i = 1; i < colsArray.length; i++) {
+  let listofObjects = {};
+  for (let j = 0; j < colsArray[i].length; j++) {
+    // Checking for which col & row
+
+    switch (colsArray[i][j]) {
+      case colsArray[i][0]:
+        listofObjects += `${headers[0]}: ${colsArray[i][j]}, `;
+        break;
+
+      case colsArray[i][1]:
+        listofObjects += `${headers[1]}: ${colsArray[i][j]}, `;
+        break;
+
+      case colsArray[i][2]:
+        listofObjects += `${headers[2]}: ${colsArray[i][j]}, `;
+        break;
+
+      case colsArray[i][3]:
+        listofObjects += `${headers[3]}: ${colsArray[i][j]}`;
+        break;
+    }
+  }
+
+  //Pushes each object into the array after the outer loop
+  directory.push(listofObjects);
+}
+
+console.log(directory);
+
+// Part 4
+// 1. Remove the last element from the sorted array.
+
+// 2. Insert the following object at index 1:
+//        { id: "48", name: "Barry", occupation: "Runner", age: "25" }
+
+// 3. Add the following object to the end of the array:
+//        { id: "7", name: "Bilbo", occupation: "None", age: "111" }
